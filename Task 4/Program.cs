54d9299aac68ecae2e04a67b531d7d57f51c6bc8
Linar <alr86@mail.ru>
2022-06-24 19:10:08 +0300

    Added task4

---
+++ Task 4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Input 1 number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 2 number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 3 number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+if (a > b)
+{
+    max = a;
+}
+else 
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("Max = " + max);
