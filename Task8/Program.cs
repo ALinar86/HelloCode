adea603acd36309388948e5155e636e320e2930f
Linar <alr86@mail.ru>
2022-06-24 20:07:50 +0300

    Added task8

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input number: ");
+int  N = Convert.ToInt32(Console.ReadLine());
+int i;
+for (i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
