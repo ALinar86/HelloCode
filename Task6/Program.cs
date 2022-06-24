e9cc63eb0c964e45ced3a5f81bdda4440502f93f
Linar <alr86@mail.ru>
2022-06-24 19:22:32 +0300

    Added task6

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write("Да, число четное");
+}
+else 
+{
+    Console.Write("Нет, нечетное");
+}
